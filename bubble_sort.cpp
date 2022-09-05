74183ad25a0583816d8357eff0abf52571ebac77
Staisha Neville <sjnevill@uwaterloo.ca>
2022-09-05 15:25:39 -0400

    first commit

---
+++ bubble_sort.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+int main();
+void bubble_sort(int *a, int n);
+
+int main() {
+  int size;
+
+  std::cout << "Enter size: ";
+  std::cin >> size;
+
+  std::cout << "Enter the " << size << " entries: " << std::endl;
+  int array[size];
+  for (int i {0}; i < size; i++){
+    std::cin >> array[i];
+  }
+
+  std::cout << std::endl;
+
+  std::cout << "ORIGINAL array:";
+  for (int i {0}; i < size; i++){
+    std::cout << array[i] << std::endl;
+  }
+
+  bubble_sort(array, size);
+
+  std::cout << std::endl;
+  std::cout << std::endl;
+
+  std::cout << "SORTED array:";
+  for (int i {0}; i < size; i++){
+    std::cout << array[i] << std::endl;
+  }
+  
+  return 0;
+}
+
+void bubble_sort (int *a, int n) {
+
+  for (int i{0}; i < n; i++){
+    for (int j{0}; j < (n - i - 1); j++) {
+      if (a[j] > a[j+1]) {
+        int tmp = a[j];
+        a[j] = a[j+1];
+        a[j+1] = tmp;
+      }
+    }
+  }
+}
